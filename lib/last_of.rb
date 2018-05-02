372c0191eacb6b8692e6f34a5b8f0116e9b58299
itggot-Lukas-Schiavone <lukas.schiavone@itggot.se>
2018-05-02 14:15:27 +0200

    skapade funktionen last_of

---
+++ lib/last_of.rb
@@ -0,0 +1,14 @@
+# Public: Takes an array and gives the last item in that array as output.
+#
+# arr - The array that is checked.
+#
+# Examples
+#  
+#   last_of([932, "z", 85])
+#   => 85
+#
+# Returns the last item in the array
+def last_of(arr)
+    output = arr[-1]
+    return output
+end
